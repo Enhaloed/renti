4bdbf0a74ac58ee33d609140d48e53f354af1e8f
Enhaloed <mount.james96@gmail.com>
2024-05-30 03:23:46 +1200

    Add static files dir

--- renti_book_club/renti_book_club/settings.py
+++ renti_book_club/renti_book_club/settings.py
@@ -151,3 +151,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'book_club/static/')
