f3e7ff27a55d64b9cfc4a53758202360baf1acef
Enhaloed <mount.james96@gmail.com>
2024-05-30 03:18:51 +1200

    Add search functionality

--- renti_book_club/book_club/views.py
+++ renti_book_club/book_club/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from .forms import SearchForm
+from .models import Book, Author, Genre
 
 
 def search_for_books(request):
@@ -8,6 +10,28 @@ def search_for_books(request):
     if request.method == 'GET':
         ctx['search_form'] = SearchForm()
     elif request.method == 'POST':
-        form = SearchForm(request.user, request.POST)
+        ctx['is_search'] = True
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search_text = form.cleaned_data['generic_text']
+            if search_text:
+                words = search_text.split(' ')
+                #  Check that each word exists in any of the fields
+                q = Q()
+                for word in words:
+                    word_q = Q()
+                    for field in ['title', 'author__name', 'genre__name']:
+                        #  Word is contained in any of the fields
+                        word_q |= Q(**{f'{field}__icontains': word})
+
+                    #  AND each word is accounted for
+                    q &= word_q
+
+                ctx['books'] = Book.objects.filter(q)
+
+            else:
+                ctx['books'] = Book.objects.all()
+
+        ctx['search_form'] = form
 
     return render(request, 'book_club/html/search.html', context=ctx)
